09b667af5ba1253e73b7448fcb617a8167319a51
Jaime Capitan <jimboy.capz@gmail.com>
2022-12-11 13:21:08 +0800

    simple calculator

---
+++ simplecalc.js
@@ -0,0 +1,50 @@
+alert("Hello! Please enter two numbers.");
+
+
+let a = Number(prompt("Please enter first number:"));
+let b = Number(prompt("Please enter second number"));
+
+let operator = prompt("Please select operation:\n + \n - \n * \n /");
+let operation;
+let result;
+
+
+
+
+switch(operator) {
+    case "+":
+        function add(a, b) {
+            return a + b;  
+        }
+        operation = "addition";
+        result = add(a,b);
+        break;
+    case "-":
+        function subtract(a, b) {
+            return a - b;  
+        }
+        operation = "subtraction"; 
+        result = subtract(a, b)
+        break;
+    case "*":
+        function multiply(a, b) {
+            return a * b;  
+        }
+        operation = "multiplication";  
+        result = multiply(a, b);
+        break;
+    case "/":
+        function divide(a, b) {
+            return a / b;  
+        }
+        operation = "division";;  
+        result = divide(a, b);
+        break;
+    default:
+        alert("Please select only the given operations");
+        break;
+}
+
+
+
+alert(`You inputted:\n ${a} as the first number,\n ${b} as the second number,\n and ${operation} for the operation.\n\n The result is ${result}.`);
